4e9ba81c11e06abe0f62e070bae31b69e1b6a140
Raviegupta <raviegupta153@gmail.com>
2024-01-08 11:33:10 +0530

    layout created

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const heading = <h1>Foodberry App</h1>
+// const heading = <h1>Foodberry App</h1>
+
+
+const AppLayout = () => {
+    return (
+        <div className='app'>
+            <h1>Foodberry</h1>
+        </div>
+    )
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+root.render(AppLayout());
