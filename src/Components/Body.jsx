75898e6fde62b1aa82311e0a1752bf8e5097db75
Raviegupta <raviegupta153@gmail.com>
2024-01-11 10:30:10 +0530

    episode 6: fetching the live api data using useEffect Hook & making our cards dynamic with live data

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,11 +1,29 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
 
   const [listOfRestaurant, setListOfRestaurant] = useState(resList);
 
+  const fetchData = async () => {
+    console.log('fetching the live api...');
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+    const json = await data.json();
+    console.log('api data is fetched, converted into json & now printing the json data ..');
+    
+    console.log(json);
+    console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    const liveResList = json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
+    setListOfRestaurant(liveResList)
+  }
+
+  useEffect(()=> {
+    fetchData();
+  }, [])
+
+  console.log('Hello')
 
   return (
     <div className="body">
