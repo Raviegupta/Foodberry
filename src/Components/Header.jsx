5850d614e176441897e50422c5f75df977e7f1e6
Raviegupta <raviegupta153@gmail.com>
2024-01-12 11:59:02 +0530

    addinh login/logout button using useState()

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 import { LOGO_URL } from '../utils/constants';
 
 const Header = () => {
+
+    const [btnName, setBtnName] = useState('Login');
+    console.log('Header is rendered');
+
     return (
         <div className='header'>
             <div className='logo-container'>
@@ -12,6 +17,14 @@ const Header = () => {
                     <li>About</li>
                     <li>Contact</li>
                     <li>Cart</li>
+                    <button className='login' onClick={()=> {
+                        // if(btnName=='Login') {
+                        //     setBtnName('Logout')  
+                        // } else {
+                        //     setBtnName('Login'); 
+                        // }
+                        btnName=='Login'?setBtnName('Logout'):setBtnName('Login');
+                    }}>{btnName}</button>
                 </ul>
             </div>
         </div>
