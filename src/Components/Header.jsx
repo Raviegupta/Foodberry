1879e91c121617285d4457ee13a6fc644e996fbe
Raviegupta <raviegupta153@gmail.com>
2024-01-13 19:05:32 +0530

    linking routes pages using Link

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { LOGO_URL } from '../utils/constants';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 
@@ -13,10 +14,10 @@ const Header = () => {
             </div>
             <div className='nav-items'>
                 <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                    <li>Cart</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                    <li><Link to="/cart">Cart</Link></li>
                     <button className='login' onClick={()=> {
                         // if(btnName=='Login') {
                         //     setBtnName('Logout')  
