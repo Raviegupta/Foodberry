b50a0ede4e4001d7f25b41f3644e4ef7ffebf578
Raviegupta <raviegupta153@gmail.com>
2024-01-15 11:14:57 +0530

    useParams Hook from react-router-dom to make dynamic restaurant

--- src/Pages/RestaurantMenu.jsx
+++ src/Pages/RestaurantMenu.jsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 import Shimmer from "../Components/Shimmer";
+import { useParams } from "react-router";
+import { MENU_API } from "../utils/constants";
 
 
 const RestaurantMenu = () => {
 
     const [resInfo, setResInfo] = useState(null);
-
+    const {resId} = useParams();
+    console.log(resId);
 
     const fetchMenu = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=425&submitAction=ENTER");
+        const data = await fetch(MENU_API + resId);
         const json = await data.json();
 
         console.log(json);
